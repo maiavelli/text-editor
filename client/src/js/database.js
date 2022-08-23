f7c0e66ef302076aee39916d6c22197cbeba5457
maiavelli <maiacamilledavis@gmail.com>
2022-08-23 19:22:50 -0400

    GET function written

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,43 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// PUT FUNCTION
+export const putDb = async (content) => {
+  // create connection to DB
+  const jateDB = await openDB('jate', 1);
+
+  // create a new transaction and specify DB and data privileges
+  const tx = jateDB.transaction('jate', 'readwrite');
+
+  // open up desired store object 
+  const store = tx.objectStore('jate');
+
+  // put content to store 
+  const request = store.put({ jate: content });
+
+  const result = await request;
+  console.log('Data successfully saved to database', result)
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// GET FUNCTION 
+export const getDb = async () => {
+  // create connection to DB
+  const jateDB = await openDB('jate', 1);
+
+  // create a new transaction, specify DB, and readonly privileges
+  const tx = jateDB.transaction('jate', 'readonly');
+
+  // open store object
+  const store = tx.objectStore('jate');
+
+  // get all data in DB
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+
+  return result?.value;
+};
 
 initdb();
